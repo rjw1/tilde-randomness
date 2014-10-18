fcb369e2f4de56d7bc41a2e9684bc02fb236d80d
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2014-10-18 12:50:06 +0100

    Ensure the OmniOS managed service repo is set up.
    The vagrant image doesnt need this but the AMI does.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 # script to get an omnios machine to the point that bundler works.
+
+# ensure that we have more than the release pkg repo
+# errors if it already exists.
+pkg set-publisher -g http://pkg.omniti.com/omniti-ms/ ms.omniti.com
+
 # install ruby 1.9.3
 pkg install ruby-19
 # pkg install will exit wth code 4 if there is nothing to do. 
